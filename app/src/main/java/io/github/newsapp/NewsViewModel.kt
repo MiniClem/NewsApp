b5d73010519f1ff72d9911b5584a790d42d76636
Clem <cmonesti@gmail.com>
2021-11-20 17:59:53 +0100

    View model

---
+++ app/src/main/java/io/github/newsapp/NewsViewModel.kt
@@ -0,0 +1,35 @@
+package io.github.newsapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.newsapp.model.News
+import io.github.newsapp.network.DataState
+import io.github.newsapp.repository.NewsRemoteDataSource
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel
+@Inject
+constructor(private val newsRemoteDataSource: NewsRemoteDataSource) : ViewModel() {
+    private val _dataState: MutableLiveData<DataState<List<News>>> = MutableLiveData()
+
+    val dataState: LiveData<DataState<List<News>>>
+        get() = _dataState
+
+    fun getNews(): LiveData<DataState<List<News>>> {
+        viewModelScope.launch {
+            newsRemoteDataSource.getNews()
+                .onEach { dataState ->
+                    _dataState.value = dataState
+                }
+                .launchIn(viewModelScope)
+        }
+        return dataState
+    }
+}
